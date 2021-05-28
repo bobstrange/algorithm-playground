5f5fbea045b577e3d93d696c2f59b38814350241
bobstrange <bob.1983.g@gmail.com>
2021-05-29 01:06:03 +0900

    refactor: refactor 01_two_sum

--- leetcode/01_two_sum/main.ts
+++ leetcode/01_two_sum/main.ts
@@ -12,17 +12,37 @@ function twoSum(nums: number[], target: number): number[] {
   //   }
   // }
 
+  // My Solution
+  // Hash Table を利用して、対応する値の lookup のオーダーを O(1) にする
+  // Two-pass Hash Table と言うらしい
   const numMap = nums.reduce((acc, cur, idx) => {
-    acc[cur] = idx
-    return acc
-  }, {})
+    acc[cur] = idx;
+    return acc;
+  }, {});
+
   for (let i = 0; i < nums.length - 1; i++) {
     const first = nums[i];
-    const diff = target - first;
+    const diff = target - first; // 解答例では complement となっていた
     const idx = numMap[diff];
 
     if (idx && i !== idx) {
+      // 解答例では Map が キーを保持するか？ と 取得された index が i と異なるかをチェックしていた
       return [i, idx];
     }
   }
-};
+  // ループを 2 回回さずに、1 回にすることもできる
+}
+
+// 1 回ループバージョン
+function toSumModified(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [i, map.get(complement) as number];
+    }
+    map.set(nums[i], i);
+  }
+
+  throw new Error("No solution");
+}
