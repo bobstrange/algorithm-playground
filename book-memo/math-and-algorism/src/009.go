50154e23b7c227d3908407882e6b91b9b80a9dc6
bobstrange <bob.1983.g@gmail.com>
2022-05-03 23:57:09 +0900

    chore: 009 sol

---
+++ book-memo/math-and-algorism/src/009.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// N 枚のカード
+// A[i]
+// 合計 S
+// d[i][j] 左から i 番目のカードまでの中から、和が j になる組み合わせが存在するなら true
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	var n, s int
+	fmt.Scan(&n, &s)
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	dp := make([][]int, n+1)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, s+1)
+		for j := 0; j <= s; j++ {
+			dp[i][j] = -10000
+		}
+	}
+	dp[0][0] = 0
+
+	for i := 1; i < n; i++ {
+		for j := 0; j <= s; j++ {
+			if j < a[i] {
+				dp[i-1][j] = dp[i][j]
+			}
+			if j >= a[i] {
+				dp[i-1][j] = max(dp[i-1][j], dp[i-1][j-a[i]])
+			}
+		}
+	}
+	fmt.Println(dp)
+}
