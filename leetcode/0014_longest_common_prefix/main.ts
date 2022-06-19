5a7da640eac2489ff0b85ad6e413c0de960653bf
bobstrange <bob.1983.g@gmail.com>
2022-06-20 01:11:43 +0900

    feat: longest common prefix solution

---
+++ leetcode/0014_longest_common_prefix/main.ts
@@ -0,0 +1,30 @@
+import { assertEquals } from "../deps.ts";
+
+function longestCommonPrefix(strs: string[]): string {
+  const minLength =
+    strs
+      .map((str) => str.length)
+      .sort((a, b) => a - b)
+      .at(0) || 0;
+
+  const prefix = [];
+
+  for (let i = 0; i < minLength; i++) {
+    // 各 str の i 番目の文字が共通であれば、prefix に追加
+    const char = strs.at(0)?.charAt(i);
+    const isDifferent = strs.some((str) => str.charAt(i) !== char);
+    if (isDifferent) {
+      break;
+    }
+    prefix.push(char);
+  }
+  return prefix.join("");
+}
+
+Deno.test("['flower', 'flow', 'flight'] returns 'fl'", () => {
+  assertEquals(longestCommonPrefix(["flower", "flow", "flight"]), "fl");
+});
+
+Deno.test("['dog', 'racecar', 'car'] returns ''", () => {
+  assertEquals(longestCommonPrefix(["dog", "racecar", "car"]), "");
+});
