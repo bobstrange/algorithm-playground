ea9ccda59371429e9b557d18cca16e71261f624f
bobstrange <bob.1983.g@gmail.com>
2022-06-20 00:47:18 +0900

    feat: roman to integer solution

---
+++ leetcode/0013_roman_to_integer/main.ts
@@ -0,0 +1,35 @@
+import { assertEquals } from "../deps.ts";
+
+type Roman = "I" | "V" | "X" | "L" | "C" | "D" | "M";
+function romanToInt(s: string): number {
+  const map = new Map<Roman, number>([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  let target = s.replace("IV", "IIII").replace("IX", "VIIII");
+  target = target.replace("XL", "XXXX").replace("XC", "LXXXX");
+  target = target.replace("CD", "CCCC").replace("CM", "DCCCC");
+
+  return target.split("").reduce((total, current) => {
+    const num = map.get(current as Roman) as number;
+    return total + num;
+  }, 0);
+}
+
+Deno.test("romanToInt('III') returns 3", () => {
+  assertEquals(romanToInt("III"), 3);
+});
+
+Deno.test("romanToInt('LVIII') returns 58", () => {
+  assertEquals(romanToInt("LVIII"), 58);
+});
+
+Deno.test("romanToInt('MCMXCIV') returns 1994", () => {
+  assertEquals(romanToInt("MCMXCIV"), 1994);
+});
