f2b8781fc7c4351a003342067a3a04629f56061c
bobstrange <bob.1983.g@gmail.com>
2023-06-29 22:38:48 +0900

    chore: length of last word

---
+++ leetcode/0058_length_of_last_word/main.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "../deps.ts";
+
+function lengthOfLastWord(s: string): number {
+  const words = s.split(" ").filter((word) => word.length > 0);
+  return words[words.length - 1].length;
+}
+
+Deno.test("lengthOfLastWord('Hello World') returns 5", () => {
+  assertEquals(lengthOfLastWord("Hello World"), 5);
+});
+
+Deno.test("   fly me   to   the moon  returns 4", () => {
+  assertEquals(lengthOfLastWord("   fly me   to   the moon  "), 4);
+});
+
+Deno.test("luffy is still joyboy returns 6", () => {
+  assertEquals(lengthOfLastWord("luffy is still joyboy"), 6);
+});
