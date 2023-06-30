3db2cb560d1cb921b5d1edc5c5dc0e0b0c1b70a5
bobstrange <bob.1983.g@gmail.com>
2023-06-30 23:14:07 +0900

    chore: with recursion

---
+++ leetcode/0070_climbing_starts/main2.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "../deps.ts";
+
+function climbStairs(n: number): number {
+  if (n < 3) {
+    return n;
+  }
+
+  return 2 * climbStairs(n - 2) + climbStairs(n - 3);
+}
+
+Deno.test("climbStairs(2) returns 2", () => {
+  assertEquals(climbStairs(2), 2);
+});
+
+Deno.test("climbStairs(3) returns 3", () => {
+  assertEquals(climbStairs(3), 3);
+});
+
+Deno.test("climbStairs(4) returns 5", () => {
+  assertEquals(climbStairs(4), 5);
+});
