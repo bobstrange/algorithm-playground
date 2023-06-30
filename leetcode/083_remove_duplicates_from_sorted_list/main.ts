0f71428bf0ffc0a27f90cf5cd6c407ab8f53c14a
bobstrange <bob.1983.g@gmail.com>
2023-06-30 23:28:05 +0900

    chore: remove duplicates from sorted list

---
+++ leetcode/083_remove_duplicates_from_sorted_list/main.ts
@@ -0,0 +1,67 @@
+import { assertEquals } from "../deps.ts";
+
+type ListNode = {
+  val: number;
+  next: ListNode | null;
+};
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (head === null) {
+    return null;
+  }
+
+  let curr = head;
+  while (curr.next !== null) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return head;
+}
+
+function arrayToListNode(arr: number[]): ListNode | null {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  const head = {
+    val: arr[0],
+    next: null,
+  };
+
+  let curr: ListNode = head;
+  for (let i = 1; i < arr.length; i++) {
+    curr.next = {
+      val: arr[i],
+      next: null,
+    };
+    curr = curr.next;
+  }
+
+  return head;
+}
+
+function listNodeToArray(head: ListNode | null): number[] {
+  const arr: number[] = [];
+  let curr = head;
+  while (curr !== null) {
+    arr.push(curr.val);
+    curr = curr.next;
+  }
+  return arr;
+}
+
+Deno.test("deleteDuplicates(1->1->2) returns 1->2", () => {
+  const input = arrayToListNode([1, 1, 2]);
+  const expected = [1, 2];
+  assertEquals(listNodeToArray(deleteDuplicates(input)), expected);
+});
+
+Deno.test("deleteDuplicates(1->1->2->3->3) returns 1->2->3", () => {
+  const input = arrayToListNode([1, 1, 2, 3, 3]);
+  const expected = [1, 2, 3];
+  assertEquals(listNodeToArray(deleteDuplicates(input)), expected);
+});
