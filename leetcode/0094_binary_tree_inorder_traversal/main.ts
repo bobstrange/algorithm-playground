bcfa7627ef3e3dadad85cd803e4d5ff882685526
bobstrange <bob.1983.g@gmail.com>
2023-07-01 23:55:30 +0900

    chore: wip binary tree inorder traversal

---
+++ leetcode/0094_binary_tree_inorder_traversal/main.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "../deps.ts";
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {}
+
+Deno.test("inorderTraversal([1,null,2,3]) returns [1,3,2]", () => {
+  const input = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+  const expected = [1, 3, 2];
+  assertEquals(inorderTraversal(input), expected);
+});
+
+Deno.test("inorderTraversal([]) returns []", () => {
+  const input = null;
+  const expected: number[] = [];
+  assertEquals(inorderTraversal(input), expected);
+});
+
+Deno.test("inorderTraversal([1]) returns [1]", () => {
+  const input = new TreeNode(1);
+  const expected = [1];
+  assertEquals(inorderTraversal(input), expected);
+});
