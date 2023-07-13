2d81a4709fcedef0fc3980634a323fa2b78d6c6c
bobstrange <bob.1983.g@gmail.com>
2023-07-13 23:29:50 +0900

    chore: symmetric tree

---
+++ leetcode/0101_symmetric_tree/main.ts
@@ -0,0 +1,54 @@
+import { assertEquals } from "../deps.ts";
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isSymmetric(root: TreeNode | null): boolean {
+  if (root === null) {
+    return true;
+  }
+
+  return isMirror(root.left, root.right);
+}
+
+function isMirror(left: TreeNode | null, right: TreeNode | null): boolean {
+  if (left === null && right === null) {
+    return true;
+  }
+
+  if (left === null || right === null) {
+    return false;
+  }
+
+  if (left.val !== right.val) {
+    return false;
+  }
+
+  return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+}
+
+Deno.test("isSymmetric([1,2,2,3,4,4,3]) returns true", () => {
+  const input = new TreeNode(
+    1,
+    new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+    new TreeNode(2, new TreeNode(4), new TreeNode(3))
+  );
+  assertEquals(isSymmetric(input), true);
+});
+
+Deno.test("isSymmetric([1,2,2,null,3,null,3]) returns false", () => {
+  const input = new TreeNode(
+    1,
+    new TreeNode(2, null, new TreeNode(3)),
+    new TreeNode(2, null, new TreeNode(3))
+  );
+  assertEquals(isSymmetric(input), false);
+});
